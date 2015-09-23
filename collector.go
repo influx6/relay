95e4dc6a266f47a638b90b0065df76f7795a4bec
Ewetumo Alexander <trinoxf@gmail.com>
2015-09-23 14:16:36 +0100

    re-arranged collectors to be more appropriate in code

--- collector.go
+++ collector.go
@@ -2,27 +2,102 @@ package relay
 
 import "sync"
 
+//StringEachfunc defines the type of the Mappable.Each rule
+type StringEachfunc func(interface{}, string, func())
+
+//Collector defines a typ of map string
+type Collector map[string]interface{}
+
+//NewCollector returns a new collector instance
+func NewCollector() Collector {
+	return make(Collector)
+}
+
+//Clone makes a new clone of this collector
+func (c Collector) Clone() Collector {
+	col := make(Collector)
+	col.Copy(c)
+	return col
+}
+
+//Remove deletes a key:value pair
+func (c Collector) Remove(k string) {
+	if c.Has(k) {
+		delete(c, k)
+	}
+}
+
+//Keys return the keys of the Collector
+func (c Collector) Keys() []string {
+	var keys []string
+	c.Each(func(_ interface{}, k string, _ func()) {
+		keys = append(keys, k)
+	})
+	return keys
+}
+
+//Get returns the value with the key
+func (c Collector) Get(k string) interface{} {
+	return c[k]
+}
+
+//Has returns if a key exists
+func (c Collector) Has(k string) bool {
+	_, ok := c[k]
+	return ok
+}
+
+//HasMatch checks if key and value exists and are matching
+func (c Collector) HasMatch(k string, v interface{}) bool {
+	if c.Has(k) {
+		return c.Get(k) == v
+	}
+	return false
+}
+
+//Set puts a specific key:value into the collector
+func (c Collector) Set(k string, v interface{}) {
+	c[k] = v
+}
+
+//Copy copies the map into the collector
+func (c Collector) Copy(m map[string]interface{}) {
+	for v, k := range m {
+		c.Set(v, k)
+	}
+}
+
+//Each iterates through all items in the collector
+func (c Collector) Each(fx StringEachfunc) {
+	var state bool
+	for k, v := range c {
+		if state {
+			break
+		}
+
+		fx(v, k, func() {
+			state = true
+		})
+	}
+}
+
+//Clear clears the collector
+func (c Collector) Clear() {
+	for k := range c {
+		delete(c, k)
+	}
+}
+
 // SyncCollector provides a mutex controlled map
 type SyncCollector struct {
 	c  Collector
 	rw sync.RWMutex
 }
 
-//Collector defines a typ of map string
-type Collector map[string]interface{}
-
-//StringEachfunc defines the type of the Mappable.Each rule
-type StringEachfunc func(interface{}, string, func())
-
 //NewSyncCollector returns a new collector instance
 func NewSyncCollector() *SyncCollector {
 	so := SyncCollector{c: make(Collector)}
 	return &so
-}
-
-//NewCollector returns a new collector instance
-func NewCollector() Collector {
-	return make(Collector)
 }
 
 //Clone makes a new clone of this collector
@@ -37,25 +112,11 @@ func (c *SyncCollector) Clone() *SyncCollector {
 	return &so
 }
 
-//Clone makes a new clone of this collector
-func (c Collector) Clone() Collector {
-	col := make(Collector)
-	col.Copy(c)
-	return col
-}
-
 //Remove deletes a key:value pair
 func (c *SyncCollector) Remove(k string) {
 	c.rw.Lock()
 	c.c.Remove(k)
 	c.rw.Unlock()
-}
-
-//Remove deletes a key:value pair
-func (c Collector) Remove(k string) {
-	if c.Has(k) {
-		delete(c, k)
-	}
 }
 
 //Set puts a specific key:value into the collector
@@ -65,20 +126,8 @@ func (c *SyncCollector) Set(k string, v interface{}) {
 	c.rw.Unlock()
 }
 
-//Set puts a specific key:value into the collector
-func (c Collector) Set(k string, v interface{}) {
-	c[k] = v
-}
-
 //Copy copies the map into the collector
 func (c *SyncCollector) Copy(m map[string]interface{}) {
-	for v, k := range m {
-		c.Set(v, k)
-	}
-}
-
-//Copy copies the map into the collector
-func (c Collector) Copy(m map[string]interface{}) {
 	for v, k := range m {
 		c.Set(v, k)
 	}
@@ -100,31 +149,8 @@ func (c *SyncCollector) Each(fx StringEachfunc) {
 	c.rw.RUnlock()
 }
 
-//Each iterates through all items in the collector
-func (c Collector) Each(fx StringEachfunc) {
-	var state bool
-	for k, v := range c {
-		if state {
-			break
-		}
-
-		fx(v, k, func() {
-			state = true
-		})
-	}
-}
-
 //Keys return the keys of the Collector
 func (c *SyncCollector) Keys() []string {
-	var keys []string
-	c.Each(func(_ interface{}, k string, _ func()) {
-		keys = append(keys, k)
-	})
-	return keys
-}
-
-//Keys return the keys of the Collector
-func (c Collector) Keys() []string {
 	var keys []string
 	c.Each(func(_ interface{}, k string, _ func()) {
 		keys = append(keys, k)
@@ -141,23 +167,12 @@ func (c *SyncCollector) Get(k string) interface{} {
 	return v
 }
 
-//Get returns the value with the key
-func (c Collector) Get(k string) interface{} {
-	return c[k]
-}
-
 //Has returns if a key exists
 func (c *SyncCollector) Has(k string) bool {
 	var ok bool
 	c.rw.RLock()
 	_, ok = c.c[k]
 	c.rw.RUnlock()
-	return ok
-}
-
-//Has returns if a key exists
-func (c Collector) Has(k string) bool {
-	_, ok := c[k]
 	return ok
 }
 
@@ -171,26 +186,11 @@ func (c *SyncCollector) HasMatch(k string, v interface{}) bool {
 	return false
 }
 
-//HasMatch checks if key and value exists and are matching
-func (c Collector) HasMatch(k string, v interface{}) bool {
-	if c.Has(k) {
-		return c.Get(k) == v
-	}
-	return false
-}
-
 //Clear clears the collector
 func (c *SyncCollector) Clear() {
 	for k := range c.c {
 		c.rw.Lock()
 		delete(c.c, k)
 		c.rw.Unlock()
-	}
-}
-
-//Clear clears the collector
-func (c Collector) Clear() {
-	for k := range c {
-		delete(c, k)
 	}
 }
